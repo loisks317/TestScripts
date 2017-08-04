7946b361ae6e213ae6cc24e340dac8f5f7999085
Lois <loisks317@users.noreply.github.com>
2017-08-04 13:39:21 -0700

    made sure it works

--- PythonFeatureTest.py
+++ PythonFeatureTest.py
@@ -82,7 +82,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     data2 , result , test_size=0.25, random_state=1)
 
 # setup the model
-workingfrom sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
 
 clf2 = RandomForestRegressor()
